fa54c0ba19a815eb3370485c60ea25e685dcb728
weneshw <weneshwolde@gmail.com>
2019-02-15 20:36:53 -0500

    Changes

---
+++ main.js
@@ -0,0 +1,15 @@
+//let name=kesete;
+let age=26;
+if(age>21){
+alert("you are alert!")
+
+}else{
+alert(" you are adult!")
+}
+let speedLimit=35;
+let currentSpeed=45;
+if(currentSpeed>speedLimit){
+    alert("take a picture")
+}else{
+    alert("keep it up")
+}
